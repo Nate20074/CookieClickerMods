a21cf25fbbb866c80f2367318088b83bba6f80c7
Nate20074 <natereaper2007@gmail.com>
2025-10-17 09:17:26 -0400

    Update building.js

--- building.js
+++ building.js
@@ -1,17 +1,17 @@
 // Define a new building
-Game.Objects['CustomBuilding'] = new Game.Object('CustomBuilding', {
-    name: 'Custom Building',
-    desc: 'A custom building that produces cookies.',
-    basePrice: 1e6, // Initial price
-    cps: 100, // Cookies per second
+Game.Objects['Cookie-Essence'] = new Game.Object('Cookie-Essence', {
+    name: 'Cookie-Essence',
+    desc: 'what have you done this is impossible you...somehow harvested the essence of the cookies you have been making this entire time to make more cookies out of those cookies and so a parodox started you harvested the cookie...Impossible!!!.',
+    basePrice: 1e30, // Initial price
+    cps: 2.9e+18, // Cookies per second
     icon: [25, 14], // Icon coordinates in the spritesheet
     buyFunction: function() {
         // Add custom behavior when purchased
-        console.log('Custom building purchased!');
+        console.log('Cookie-Essence purchased!');
     }
 });
 
 // Add the building to the game
-Game.Objects['CustomBuilding'].unlock();
-Game.Objects['CustomBuilding'].draw();
+Game.Objects['Cookie-Essence'].unlock();
+Game.Objects['Cookie-Essence'].draw();
 Game.RefreshBuildings();
